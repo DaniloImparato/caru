ea65ed225af8d0eadebea4c23bdb910f3710f979
Danilo Imparato <imparatodanilo@gmail.com>
2018-08-13 01:24:50 -0300

    upgrade to firebase cloud functions v2

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,30 @@
-// Import the Firebase SDK for Google Cloud Functions.
 const functions = require('firebase-functions');
-
-// Import and initialize the Firebase Admin SDK.
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+var serviceAccount = require('./secret/serviceAccountKey.json');
 
-// Adds a message that welcomes new users into the chat.
-exports.addVerificationField = functions.auth.user().onCreate(event => {
-  const user = event.data;
-  console.log('A new user signed in for the first time.');
-
-  // Adds the field
-  return admin.database().ref('users/' + user.uid + '/verified').set(false);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://caru-47212.firebaseio.com"
 });
 
-exports.makeUppercase = functions.database.ref('/cards/{pushId}/userid').onWrite(event => {    
-    //var isAdmin = event.auth.admin;
-    var uid = event.auth.variable ? event.auth.variable.uid : null;
+// Adiciona o campo verified no usuario.
+exports.addVerificationField = functions.auth.user().onCreate((user, context) => {
+    return admin.database().ref('users/' + user.uid + '/verified').set(false);
+});
 
-    const userid = event.data.val();
-    console.log('Logging', event.params.pushId, userid);
-    
-    admin.database().ref('/log').push(
-        {
-            cardid: event.params.pushId,
+// Log
+exports.addLogEntry = functions.database.ref('/cards/{pushId}/userid').onWrite((change, context) => {
+    var uid = context.auth ? context.auth.uid : 'admin';
+
+    const userid = change.after.val();
+
+    return admin.database().ref('/log').push({
+            cardid: context.params.pushId,
             triggerer: uid,
             userid: userid,
             timestamp: admin.database.ServerValue.TIMESTAMP
-        }
-        ).then(snapshot => {
-        console.log('Logged');
+        }).then(snapshot => {
+            console.log("Logged card " + context.params.pushId);
     });
 });
